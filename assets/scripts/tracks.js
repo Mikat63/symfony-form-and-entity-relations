0dcc42fdb5d091c3668eb139c22c4fa3d467e8b9
mikaT <m.tores63@outlook.fr>
2026-03-16 14:47:03 +0100

    COMPLETED : card and components ok

---
+++ assets/scripts/tracks.js
@@ -0,0 +1,4 @@
+const addTrackBtn = document.querySelector(".add-track-btn");
+const editTrackBtn = document.querySelectorAll('.edit-track-btn');
+const deleteTrackBtn = document.querySelectorAll('.delete-track-btn');
+
